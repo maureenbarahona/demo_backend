ce8400843486475f414f5adb297837b7926087a3
Maureen Barahona <maureenbarahona@gmail.com>
2020-09-25 11:35:30 -0600

    datos de pruebas

--- app/controllers/quotation.controller.js
+++ app/controllers/quotation.controller.js
@@ -1,9 +1,9 @@
 const Quotation = require ('../models/quotation.model');
 
 exports.create=(req, res) => {
-    if(!req.body.contect){
+    if(!req.body.valor){
         return res.status(400).send({
-            message: "Quotation content can not be empty"
+            message: "Quotation valor can not be empty"
         });
     }
 
@@ -25,19 +25,51 @@ exports.create=(req, res) => {
 };
 
 exports.findAll = (req, res) => {
+    Quotation.find()
+    .then(quotation =>{
+        res.send(quotation);
+    }).catch(err => {
+        res.status(500).send({
+            message: err.message|| "Some error occurred wile creating the Quotation"
+        });
+    });
 
 };
 
 exports.findOne = (req, res) =>{
+    Quotation.findById(req.params.moneda)
+    .then(quotation =>{
+        if(!quotation){
+            return res.status(404).send({
+                message:  "Quotation not found with id"+ req.params.moneda  
+            });
+        }
+        res.send(quotation);
+    }).catch(err => {
+        if(err.kind === 'ObjectId'){
+            return res.status(404).send({
+                message: "Quotation not found with moneda "+ req.params.moneda
+            });
+        }
+        return res.status(500).send({
+            message: "Error retrieving quotation with id" + req.params.moneda
+        });
+    })
 
 };
 
-exports.findOne =(req, res) =>{
-
-
-};
 
 exports.update =(req, res )=> {
+    if(!req.body.valor){
+        return res.status(400).send({
+            message: "Quotation valor can not be empty"
+        });
+    }
+
+    Quotation.findByIdAndUpdate(req.params.moneda, {
+        moneda: req.body.moneda ||"Sin moneda",
+        valor: req.body.valor
+    })
 
 };
 
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const dbConfig = require('./config/database.config');
-require ('./app/routes/quotation.routes.js');
+//require ('./app/routes/quotation.routes.js');
 
 const app = express();
 
@@ -24,6 +24,22 @@ mongoose.connect(dbConfig.url, {
 app.get('/', (req, res) =>{
     res.json({"message":"welcome to my MauDemo"})
 });
+
+app.get('/cotizacion/dolar', (req, res)=>{
+    res.json({  "moneda": "dolar",
+        "precio": "38.2"
+    })
+})
+app.get('/cotizacion/euro', (req, res)=>{
+    res.json({  "moneda": "euro",
+        "precio": "58.2"
+    })
+})
+app.get('/cotizacion/real', (req, res)=>{
+    res.json({  "moneda": "real",
+        "precio": "78.2"
+    })
+})
 
 app.listen(3000,() =>{
     console.log("server is listening on port 3000");
