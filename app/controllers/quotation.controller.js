3e26ee292e4f7e29d99ebda7423af7f960fa9fbd
Maureen Barahona <maureenbarahona@gmail.com>
2020-09-25 11:06:37 -0600

    controller, create quotation

--- app/controllers/quotation.controller.js
+++ app/controllers/quotation.controller.js
@@ -1,6 +1,26 @@
 const Quotation = require ('../models/quotation.model');
 
 exports.create=(req, res) => {
+    if(!req.body.contect){
+        return res.status(400).send({
+            message: "Quotation content can not be empty"
+        });
+    }
+
+    const quotation = new Quotation({
+        moneda: req.body.moneda ||"Sin moneda",
+        valor: req.body.valor
+
+    });
+
+    quotation.save()
+    .then(data =>{
+        res.send(data);
+    }).catch(err => {
+        res.status(500).send({
+            message:err.message || "Some error occurred wile creating the Quotation"
+        });
+    });
 
 };
 
